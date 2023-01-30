246911cb7ae705dea4ec116ae0ae0c6418ecede0
Willy Oliveira <willy.oliveira@acct.global>
2023-01-30 13:53:04 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import Login from "pages/Login";
+import Feira from "pages/Feira";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Carrinho from "pages/Carrinho";
+import { UsuarioProvider } from "common/context/Usuario";
+import { CarrinhoProvider } from "common/context/Carrinho";
+import { PagamentoProvider } from "common/context/Pagemento";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <UsuarioProvider>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <CarrinhoProvider>
+            <PagamentoProvider>
+              <Route path="/feira">
+                <Feira />
+              </Route>
+              <Route path="/carrinho">
+                <Carrinho />
+              </Route>
+            </PagamentoProvider>
+          </CarrinhoProvider>
+        </UsuarioProvider>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
